b87c32fe2ca99302e007a4078092cc3907d9f236
Joselito Coutinho <joselitocoutinho92@gmail.com>
2023-11-15 08:20:47 -0100

    Refactor logging and add documentation to functions

--- utils.py
+++ utils.py
@@ -8,27 +8,53 @@ from dotenv import load_dotenv
 from plyer import notification
 import logging
 
-# Load environment variables from a .env file.
+# Load environment variables from a .env file for configuration management.
 load_dotenv()
 
-# Function to set up the logging configuration.
-# Allows specification of the log level and the filename for the log file.
-def setup_logging(level=logging.DEBUG, filename='app.log'):
-    logging.basicConfig(level=level, filename=filename, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Function to establish a database connection.
-# Takes the database file path as an argument and returns the connection object.
+def setup_logging(level=logging.DEBUG, filename='app.log'):
+    """
+    Set up the logging configuration for the application.
+
+    Args:
+        level: The logging level (e.g., DEBUG, INFO).
+        filename: The name of the file where logs will be stored.
+    """
+    logging.basicConfig(level=level, filename=filename,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 def get_db_connection(db_file):
+    """
+    Establish a database connection.
+
+    Args:
+        db_file: The file path of the database.
+
+    Returns:
+        A connection object to the SQLite database.
+
+    Raises:
+        sqlite3.Error: If a connection error occurs.
+    """
     try:
         return sqlite3.connect(db_file)
     except sqlite3.Error as e:
-        # Logs any database connection errors and re-raises the exception.
         logging.error(f"Database connection error: {e}")
         raise
 
-# Function to hash a password using SHA-256.
-# Takes a password and an optional salt. If no salt is provided, it generates one.
+
 def hash_password(password, salt=None):
+    """
+    Hash a password using SHA-256.
+
+    Args:
+        password: The password to hash.
+        salt: An optional salt for hashing. If not provided, a new salt is generated.
+
+    Returns:
+        A tuple of the hashed password and the used salt.
+    """
     if salt is None:
         salt = hashlib.sha256(os.urandom(32)).hexdigest()
     password_bytes = password.encode('utf-8')
@@ -37,78 +63,151 @@ def hash_password(password, salt=None):
     hashed_password = hashlib.sha256(password_salt).hexdigest()
     return hashed_password, salt
 
-# Function to validate a username.
-# Ensures that the username is at least 4 characters long.
+
 def is_valid_username(username):
+    """
+    Validate a username.
+
+    Args:
+        username: The username to validate.
+
+    Returns:
+        True if the username is at least 4 characters long, False otherwise.
+    """
     return len(username) >= 4
 
+
 def is_valid_task_name(task_name):
-    # Example: Task name should not be empty
+    """
+    Validate a task name.
+
+    Args:
+        task_name: The task name to validate.
+
+    Returns:
+        True if the task name is not empty, False otherwise.
+    """
     return len(task_name.strip()) > 0
 
-# Function to validate a password.
-# Checks that the password meets certain criteria using regular expressions.
+
 def is_valid_password(password):
-    password_pattern = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$')
+    """
+    Validate a password against a set of criteria.
+
+    Args:
+        password: The password to validate.
+
+    Returns:
+        True if the password meets the criteria, False otherwise.
+    """
+    password_pattern = re.compile(
+        r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$')
     return bool(password_pattern.match(password))
 
-# Function to validate an email address.
-# Uses a regular expression to ensure the email is in a proper format.
+
 def is_valid_email(email):
+    """
+    Validate an email address format.
+
+    Args:
+        email: The email address to validate.
+
+    Returns:
+        True if the email is in a proper format, False otherwise.
+    """
     pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
     return bool(pattern.match(email))
 
-# Function to retrieve an environment variable.
-# Takes the variable name and an optional default value.
+
 def get_env_variable(var_name, default=None):
+    """
+    Retrieve an environment variable.
+
+    Args:
+        var_name: The name of the environment variable.
+        default: An optional default value if the variable is not found.
+
+    Returns:
+        The value of the environment variable or the default value.
+    """
     return os.getenv(var_name, default)
 
-# Function to format a QDateTime object into a string.
-# Allows specification of the format.
+
 def format_datetime(date_time, format="yyyy-MM-dd HH:mm:ss"):
+    """
+    Format a QDateTime object into a string.
+
+    Args:
+        date_time: The QDateTime object to format.
+        format: The format string.
+
+    Returns:
+        A formatted string representation of the date and time.
+    """
     return date_time.toString(format)
 
-# Function to parse a string into a QDateTime object.
-# Takes a date-time string and the format it's in.
+
 def parse_datetime(date_time_str, format="%Y-%m-%d %H:%M:%S"):
+    """
+    Parse a string into a QDateTime object.
+
+    Args:
+        date_time_str: The date-time string to parse.
+        format: The format of the date-time string.
+
+    Returns:
+        A QDateTime object.
+    """
     return QDateTime.fromString(date_time_str, format)
 
-# Function to display an error message box using PyQt6.
-# Takes the title and the message as arguments.
+
 def show_error(title, message):
+    """
+    Display an error message box.
+
+    Args:
+        title: The title of the message box.
+        message: The error message to display.
+    """
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Icon.Critical)
     msg.setWindowTitle(title)
     msg.setText(message)
     msg.exec()
 
-# Function to display a general message box using PyQt6.
-# Takes the title and the message as arguments.
+
 def show_message(title, message):
+    """
+    Display a general message box.
+
+    Args:
+        title: The title of the message box.
+        message: The message to display.
+    """
     msg = QMessageBox()
     msg.setWindowTitle(title)
     msg.setText(message)
     msg.exec()
 
-# Send a Windows notification asynchronously and log the action.
+
 def send_windows_notification(title: str, message: str, task_manager, timeout: int = 10, app_name: str = 'YourApp') -> bool:
     """
     Send a Windows notification if the user has enabled notifications.
 
     Args:
-        title (str): The title of the notification.
-        message (str): The message content of the notification.
+        title: The title of the notification.
+        message: The message content of the notification.
         task_manager: Instance of TaskManager to retrieve user preferences.
-        timeout (int): The time in seconds for the notification to disappear.
-        app_name (str): The name of your application.
+        timeout: The time in seconds for the notification to disappear.
+        app_name: The name of your application.
 
     Returns:
-        bool: True if the notification was sent successfully, False otherwise.
+        True if the notification was sent successfully, False otherwise.
     """
     try:
-        # Retrieve notification preference from user settings
         preferences = task_manager.get_preferences()
-        enable_notifications = preferences.get('enable_notifications', 'True') == 'True'
+        enable_notifications = preferences.get(
+            'enable_notifications', 'True') == 'True'
 
         if enable_notifications:
             notification.notify(
@@ -127,5 +226,3 @@ def send_windows_notification(title: str, message: str, task_manager, timeout: i
     except Exception as e:
         logging.error(f"Error sending Windows notification: {str(e)}")
         return False
-
-
