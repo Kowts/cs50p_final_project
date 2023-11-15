dca5d1c43dec14480395fa9dd548d7c669eb8d47
Joselito Coutinho <joselitocoutinho92@gmail.com>
2023-11-15 08:59:58 -0100

    Refactor code to improve readability and performance.

--- task_manager.py
+++ task_manager.py
@@ -7,29 +7,38 @@ import datetime
 import logging
 from enum import Enum
 
-# Setup logging as soon as possible, ideally at the start of the application
+# Initialize logging configuration at application startup
 utils.setup_logging()
 
-# Constants
+# Constants for database file and default values, loaded from environment variables
 DATABASE_FILE = utils.get_env_variable('DATABASE_FILE')
 DEFAULT_PRIORITIES = utils.get_env_variable('DEFAULT_PRIORITIES').split(',')
 DEFAULT_CATEGORIES = utils.get_env_variable('DEFAULT_CATEGORIES').split(',')
 
 class DefaultStatus(Enum):
+    """Enum for default status values."""
     ACTIVE = 1
     INACTIVE = 0
 class TaskManager:
 
     """
-    A class to manage tasks and user authentication.
+    Manages tasks, user authentication, and database interactions.
     """
 
     def __init__(self, db_file=DATABASE_FILE):
+        """
+        Initializes the TaskManager with a database file and sets up the database.
+        Validates required environment variables.
+        """
         self.db_file = db_file
         self.setup_database()
         self.validate_environment_variables()
 
     def validate_environment_variables(self):
+        """
+        Validates required environment variables and raises ValueError if any are missing or invalid.
+        """
+        # List of required environment variables
         required_vars = ['DATABASE_FILE', 'MAX_CONNECTION', 'DEFAULT_USER', 'DEFAULT_PASSWORD']
         missing_vars = [var for var in required_vars if not utils.get_env_variable(var)]
         if missing_vars:
@@ -51,9 +60,10 @@ class TaskManager:
 
     def setup_database(self):
         """
-        Sets up the database and creates necessary tables.
+        Sets up the database by creating necessary tables.
         """
         try:
+            # Establish database connection and create tables
             with utils.get_db_connection(self.db_file) as conn:
                 self.create_tasks_table(conn)
                 self.create_priorities_table(conn)
@@ -67,9 +77,8 @@ class TaskManager:
 
 
     def create_tasks_table(self, conn):
-        """
-        Creates the tasks table in the database.
-        """
+        """Creates the tasks table in the database."""
+        # SQL command for creating the tasks table
         conn.execute('''
             CREATE TABLE IF NOT EXISTS tasks (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -166,7 +175,14 @@ class TaskManager:
                 cursor.execute("INSERT INTO categories (name, created_at) VALUES (?, ?)", (category, current_time))
 
     def load_priorities(self):
+        """
+        Loads priorities from the database.
+
+        Returns:
+            A list of priorities if successful, an empty list otherwise.
+        """
         try:
+            # [Database query logic]
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
                 cursor.execute('SELECT name FROM priorities')
@@ -184,6 +200,12 @@ class TaskManager:
             return []
 
     def load_categories(self):
+        """
+        Retrieves a list of category names from the database.
+
+        Returns:
+            A list of category names if the query is successful, an empty list otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -191,17 +213,21 @@ class TaskManager:
                 categories = [row[0] for row in cursor.fetchall()]
                 return categories
         except sqlite3.DatabaseError as e:
-            # Handle specific database-related errors
+            # Logs database-related errors and provides feedback for debugging
             logging.error(f"Database error: {e}")
-            # Consider logging this error and returning an appropriate response
-            return []
+            return []  # Returns an empty list to indicate failure in category retrieval
         except Exception as e:
-            # Handle other, more general exceptions
+            # Captures and logs unexpected errors
             logging.error(f"An error occurred: {e}")
-            # Again, consider logging and how you want to handle this in the UI
-            return []
+            return []  # Returns an empty list as a fallback
 
     def get_existing_users(self):
+        """
+        Retrieves a list of existing usernames from the database.
+
+        Returns:
+            A list of usernames if the query is successful, an empty list otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -209,17 +235,25 @@ class TaskManager:
                 users = [row[0] for row in cursor.fetchall()]
                 return users
         except sqlite3.DatabaseError as e:
-            # Handle specific database-related errors
+            # Handles database-specific errors with logging for troubleshooting
             logging.error(f"Database error: {e}")
-            # Consider logging this error and returning an appropriate response
-            return []
+            return []  # Returns an empty list to indicate an issue in retrieving users
         except Exception as e:
-            # Handle other, more general exceptions
+            # Deals with general exceptions, ensuring the application remains stable
             logging.error(f"An error occurred: {e}")
-            # Again, consider logging and how you want to handle this in the UI
-            return []
+            return []  # Provides an empty list as a default response
 
     def create_user(self, username, password):
+        """
+        Creates a new user in the database.
+
+        Args:
+            username: The username of the new user.
+            password: The password of the new user.
+
+        Returns:
+            None if successful, an error message otherwise.
+        """
         if not utils.is_valid_username(username) or not utils.is_valid_password(password):
             raise ValueError("Invalid username or password")
 
@@ -237,6 +271,16 @@ class TaskManager:
             return str(e)
 
     def verify_user(self, username, password):
+        """
+        Verifies a user's credentials.
+
+        Args:
+            username: The username of the user.
+            password: The password of the user.
+
+        Returns:
+            True if credentials are valid, False otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -255,6 +299,18 @@ class TaskManager:
             return False
 
     def add_task(self, task_name, due_date, priority, category):
+        """
+        Adds a new task to the database.
+
+        Args:
+            task_name: The name of the task.
+            due_date: The due date of the task.
+            priority: The priority of the task.
+            category: The category of the task.
+
+        Returns:
+            None and the task ID if successful, an error message and None otherwise.
+        """
         if not utils.is_valid_task_name(task_name):
             raise ValueError("Invalid task name.")
 
@@ -272,6 +328,19 @@ class TaskManager:
             return str(e), None
 
     def update_task(self, task_id, name, due_date, priority, category):
+        """
+        Updates an existing task in the database.
+
+        Args:
+            task_id: The unique identifier of the task.
+            name: The updated name of the task.
+            due_date: The updated due date of the task.
+            priority: The updated priority of the task.
+            category: The updated category of the task.
+
+        Returns:
+            None if successful, an error message otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -279,66 +348,74 @@ class TaskManager:
         except sqlite3.Error as e:
             logging.error(f"Database error while updating task: {e}")
             return "Failed to update task."
-        return None
+        return None  # Indicates successful update
 
     def get_task_details(self, task_id):
+        """
+        Retrieves details of a specific task.
+
+        Args:
+            task_id: The unique identifier of the task.
+
+        Returns:
+            A tuple containing task details if successful, None otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
                 cursor.execute("SELECT id, name, due_date, priority, category FROM tasks WHERE id = ?", (task_id,))
-                return cursor.fetchone()
+                return cursor.fetchone()  # Returns a single task's details
         except sqlite3.Error as e:
             logging.error(f"Database error while retrieving task details: {e}")
             return None
 
     def list_tasks(self, status=None):
+        """
+        Lists tasks based on their status.
+
+        Args:
+            status: The status of the tasks to list. If None, lists active tasks.
+
+        Returns:
+            A list of tasks matching the given status, empty list in case of an error.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
-                if status is None:
-                    cursor.execute('SELECT id, name, due_date, priority, category FROM tasks WHERE status = ?', (DefaultStatus.ACTIVE.value,))
-                else:
-                    cursor.execute('SELECT id, name, due_date, priority, category FROM tasks WHERE status = ?', (status,))
-                return cursor.fetchall()
+                query = 'SELECT id, name, due_date, priority, category FROM tasks WHERE status = ?'
+                cursor.execute(query, (status or DefaultStatus.ACTIVE.value,))
+                return cursor.fetchall()  # Returns a list of tasks
         except sqlite3.DatabaseError as e:
-            # Handle specific database-related errors
             logging.error(f"Database error: {e}")
-            # Consider logging this error and returning an appropriate response
             return []
         except Exception as e:
-            # Handle other, more general exceptions
             logging.error(f"An error occurred: {e}")
-            # Again, consider logging and how you want to handle this in the UI
             return []
 
     def remove_task(self, task_id):
+        """
+        Sets a task's status to inactive, effectively removing it from active listings.
+
+        Args:
+            task_id: The unique identifier of the task to be removed.
+
+        Returns:
+            None if successful, an error message otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
                 cursor.execute('UPDATE tasks SET status = ? WHERE id = ?', (DefaultStatus.INACTIVE.value, task_id,))
-            return None
         except sqlite3.Error as e:
-            return str(e)
-
-    def remove_tasks(self, task_ids):
-        """
-        Set multiple tasks as inactive in the database.
-
-        :param task_ids: A list of task IDs to be set as inactive.
-        """
-        try:
-            with sqlite3.connect(self.db_file) as conn:
-                cursor = conn.cursor()
-                # Create a query string with the correct number of placeholders
-                placeholders = ', '.join(['?'] * len(task_ids))
-                query = f"UPDATE tasks SET status = {DefaultStatus.INACTIVE.value} WHERE id IN ({placeholders})"
-                cursor.execute(query, task_ids)
-                conn.commit()
-            return "Tasks successfully set as inactive."
-        except sqlite3.Error as e:
-            return str(e)
+            return str(e)  # Returns the error message in case of failure
 
     def get_last_inserted_task_id(self):
+        """
+        Retrieves the ID of the last inserted task in the database.
+
+        Returns:
+            The ID of the last inserted task if successful, None otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -346,9 +423,15 @@ class TaskManager:
                 task_id = cursor.fetchone()[0]
             return task_id
         except sqlite3.Error:
-            return None
+            return None  # Return None if there's an error during the operation
 
     def get_due_tasks(self):
+        """
+        Retrieves tasks that are due on the current day.
+
+        Returns:
+            A list of task names due today, an empty list in case of an error.
+        """
         today = datetime.date.today().strftime("%Y-%m-%d")
         try:
             with utils.get_db_connection(self.db_file) as conn:
@@ -365,6 +448,15 @@ class TaskManager:
             return []
 
     def export_tasks(self, file_path):
+        """
+        Exports active tasks to a CSV file.
+
+        Args:
+            file_path: The file path where the tasks should be exported.
+
+        Returns:
+            A success message if the export is successful, an error message otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
@@ -373,16 +465,24 @@ class TaskManager:
 
             with open(file_path, mode='w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                # Write the headers
                 writer.writerow(['ID', 'Name', 'Due Date', 'Priority', 'Category', 'Created At', 'Status'])
                 for task in tasks:
                     writer.writerow(task)
 
             return "Tasks exported successfully."
         except Exception as e:
-            return f"Error exporting tasks: {e}"
+            return f"Error exporting tasks: {e}"  # Return error message in case of failure
 
     def import_tasks(self, file_name):
+        """
+        Imports tasks from a CSV file into the database.
+
+        Args:
+            file_name: The path to the CSV file containing tasks.
+
+        Returns:
+            A success message if the import is successful, an error message otherwise.
+        """
         try:
             with open(file_name, mode='r') as file:
                 reader = csv.reader(file)
@@ -390,38 +490,44 @@ class TaskManager:
                 with sqlite3.connect(self.db_file) as conn:
                     cursor = conn.cursor()
                     for row in reader:
-                        # Extract required elements from each row
+                        # Ensure each row has the required number of elements
                         if len(row) >= 5:
-                            task_name = row[1]   # Second element (index 1)
-                            due_date = row[2]    # Third element (index 2)
-                            priority = row[3]    # Fourth element (index 3)
-                            category = row[4]    # Fifth element (index 4)
+                            task_name, due_date, priority, category = row[1:5]
 
-                            # Check if task name is valid
+                            # Validate the task name
                             if not utils.is_valid_task_name(task_name):
                                 raise ValueError(f"Invalid task name: {task_name}")
 
-                            # Insert into the database
+                            # Prepare other task details and insert into the database
                             created_at = QDateTime.currentDateTime().toString("yyyy-MM-dd hh:mm:ss")
                             cursor.execute(
                                 "INSERT INTO tasks (name, due_date, priority, category, created_at, status) VALUES (?, ?, ?, ?, ?, ?)",
-                                (task_name, due_date, priority, category, created_at, DefaultStatus.ACTIVE.value)
+                                (task_name, due_date, priority, category,
+                                created_at, DefaultStatus.ACTIVE.value)
                             )
                         else:
                             print(f"Skipping incomplete row: {row}")
             return "Import successful"
         except Exception as e:
+            # Error handling with detailed message
             return f"Import failed: {str(e)}"
 
+
     def get_preferences(self):
-        # Retrieve preferences from the database
+        """
+        Retrieves user preferences from the database.
+
+        Returns:
+            A dictionary of preferences if successful, an empty dictionary otherwise.
+        """
         try:
             with sqlite3.connect(self.db_file) as conn:
                 cursor = conn.cursor()
                 cursor.execute("SELECT key, value FROM preferences")
+                # Create a dictionary from the fetched preferences
                 return {key: value for key, value in cursor.fetchall()}
         except sqlite3.Error as e:
-            return {}
+            return {}  # Returns an empty dictionary in case of an error
 
     def save_preferences(self, preferences):
         """
