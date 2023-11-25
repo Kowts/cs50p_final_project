deb04d4d913304bfd0da88bc2dd133f2014bea1c
Joselito Coutinho <joselitocoutinho92@gmail.com>
2023-11-25 22:29:30 -0100

    Add constants for theme, font size, and regular expressions

--- helpers/constants.py
+++ helpers/constants.py
@@ -8,3 +8,13 @@ DEFAULT_CATEGORIES = get_env_variable('DEFAULT_CATEGORIES').split(',')
 # Constants for status
 STATUS_ACTIVE = 1
 STATUS_INACTIVE = 0
+
+# Constants for theme and font size
+THEME = get_env_variable('THEME').split(',')
+FONT_SIZE = get_env_variable('FONT_SIZE').split(',')
+
+# Centralized configuration for regular expressions
+REGEX_PATTERNS = {
+    'password': r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()]).{8,}$',
+    'email': r"[^@]+@[^@]+\.[^@]+"
+}
